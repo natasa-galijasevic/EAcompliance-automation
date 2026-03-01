7850ab3202a843d419155558412c4b23b034b012
natasa-galijasevic <natasa.galijasevic@raiffeisengroup.ba>
2026-03-01 19:42:32 +0100

    Update EA_agent.py

--- EA_agent.py
+++ EA_agent.py
@@ -11,11 +11,11 @@ response = requests.get(
   f"{base_url}/{page_id}?expand=body.storage",
   auth=HTTPBasicAuth(email, api_token)
   )
-  if response.status_code == 200:
+if response.status_code == 200:
     page = response.json()
     title = page["title"]
     content = page["body"]["storage"]["value"]
     print(f"Page title: {title}\n")
     print(f"Content (prvoh 500 karaktera):\n{content[:500]}...")
-  else:
+else:
     print(f"Failed to fetch page: {response.status_code} - {response.text}")
