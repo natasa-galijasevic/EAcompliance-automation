b6e2e9e5c7b16d4a4e89fe49da62794d368d8456
natasa-galijasevic <natasa.galijasevic@raiffeisengroup.ba>
2026-03-01 18:36:18 +0100

    Update EA_agent.py

--- EA_agent.py
+++ EA_agent.py
@@ -1,1 +1,20 @@
-print("agent is alive")
+import requests
+from requests.auth import HTTPBasicAuth
+
+base_url=""
+page_id=""
+email ="natasa.galijasevic.raiffeisengroup.ba"
+api_tokem = ""
+
+response = requests.get(
+  f"{base_url}/{page_id}?expand=body.storage",
+  auth=HTTPBasicAuth(email, api_token)
+  )
+  if response.status_code == 200:
+    page = response.json()
+    title = page["title"]
+  content = page["body"]["storage"]["value"]
+    print(f"Page title: {title}\n")
+    print(f"Content (prvoh 500 karaktera):\n{content[:500]}...")
+  else:
+    print(f"Failed to fetch page: {response.status_code} - {response.text}")
