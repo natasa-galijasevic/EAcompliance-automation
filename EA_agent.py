04556935f32f73f537afac4b2b18b5fc730d73a0
natasa-galijasevic <natasa.galijasevic@raiffeisengroup.ba>
2026-03-02 01:56:07 +0100

    Update EA_agent.py

--- EA_agent.py
+++ EA_agent.py
@@ -1,27 +1,16 @@
-import requests
-from requests.auth import HTTPBasicAuth
+from atlassian import Confluence
 
-base_url="https://wiki.rbinternational.com/confluence/rest/api/content"
-page_id="5330309263"
-email ="natasa.galijasevic@raiffeisengroup.ba"
-import os
-api_token=os.environ.get("ConfluenceToken")
+url="https://wiki.rbinternational.com/confluence"
+username = "IBAASNA"
+token = "ConfluenceToken"
 
-response = requests.get(
-  f"{base_url}/{page_id}?expand=body.storage",
-  auth=HTTPBasicAuth(email, api_token)
-  )
-# if response.status_code == 200:
-    # page = response.json()
-    # title = page["title"]
-    # content = page["body"]["storage"]["value"]
-    # print(f"Page title: {title}\n")
-    # print(f"Content (prvoh 300 karaktera):\n{content[:500]}...")
-# else:
-    # print(f"Failed to fetch page: {response.status_code} - {response.text}")
+confluence = Confluence(
+  url=url,
+  username=username,
+  password=token
+)
 
-print("STATUS CODE:")
-print(response.status_code)
+space_key="RBBHIT"
+space = confluence.get_space(space_key)
+print(space)
 
-print("RESPONSE TEXT:")
-print(response.text[:500])
