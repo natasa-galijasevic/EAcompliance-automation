79dd5c47807f1c9f0f61fec62f6ef8bb2bcd7e29
natasa-galijasevic <natasa.galijasevic@raiffeisengroup.ba>
2026-03-05 20:06:56 +0100

    Update EA_agent.py

--- EA_agent.py
+++ EA_agent.py
@@ -1,16 +1,21 @@
+
 from atlassian import Confluence
 
-url="https://wiki.rbinternational.com/confluence"
-username = "IBAASNA"
-token = "ConfluenceToken"
+url = "https://wiki.rbinternational.com/confluence"
+username = "natasa.galijasevic@raiffeisengroup.ba"   # PAT zahtijeva e-mail
+token = "ConfluenceToken"                             # pravi Confluence API token
 
 confluence = Confluence(
-  url=url,
-  username=username,
-  password=token
+    url=url,
+    username=username,
+    password=token,
+    verify_ssl=False,  # čest problem u internim mrežama
 )
 
-space_key="RBBHIT"
-space = confluence.get_space(space_key)
-print(space)
+space_key = "RBBHIT"
 
+try:
+    space = confluence.get_space(space_key)
+    print(space)
+except Exception as e:
+    print("Error:", e)
